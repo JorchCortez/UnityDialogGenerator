fb079683ec52d7264aeaa73c9e342731614fdbe1
Joorch <jacortez_94@hotmail.com>
2020-01-29 17:26:51 -0600

    adding first non functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Dialog from './components/dialog';
 import logo from './logo.svg';
 import './App.css';
 
@@ -11,7 +12,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <Dialog />
         </p>
       </div>
     );
---
+++ src/components/dialog.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Dialog extends React.Component {
+  render() {
+    return (
+    <div className="conversation">
+    <div className="line">
+        <label>Character</label>
+        <input type="text" />
+        <label>Dialog</label>
+        <textarea rows="5" cols="50"></textarea>
+    </div>
+    </div>
+    );
+  }
+}
+
+export default Dialog;
