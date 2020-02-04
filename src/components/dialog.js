aae3cce5fdd6c2426172ea7272bc86b0268c692d
Joorch <jacortez_94@hotmail.com>
2020-02-04 13:24:55 -0600

    finishing the add new items to list button

--- src/components/dialog.js
+++ src/components/dialog.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import Line from './line'
-class Dialog extends React.Component {
- 
-    DeleteLine = () =>{ 
-        console.log("Aaaaaaaand it's gone")
+class Dialog extends React.Component { 
+    constructor(props) {
+        super(props);
+         this.state = {
+                 lines: [{
+                     character: "",
+                     dialog:"",
+                     emotion:""
+                 }]
+             }
+     } 
+
+    DeleteLine = (lineIndex) =>{ 
+        const dialog = [...this.state.lines]; 
+        console.table(dialog);
+        dialog.splice(lineIndex, 1);
+        console.table(dialog);
+        console.log(lineIndex);
+        this.setState({lines: dialog})
     }
 
-    AddLine = () =>{ 
+    AddLine = () =>{  
+
+        var newLine = { character: "",dialog:"",emotion:""}
+
+        this.setState({ lines: [newLine, ...this.state.lines]
+    }) 
         console.log("Adding line", "yeah im supposed to ain't i?")
     }
 
@@ -14,12 +34,22 @@ class Dialog extends React.Component {
         console.log( "Yoooup this is supposed to work at some point")
     }
 
+    GetCharacter = (e) => {
+        console.log(e.target.value)
+    }
+    GetText = (e) => {
+        this.setState({itemDescription: e.target.value});
+    }
+    GetEmotion = (e) => {
+        this.setState({itemPrice: parseInt(e.target.value)});
+    }
+
 
   render() {
     return (
         <div className="conversation">
         <div className="dialogs">
-            <Line DeleteLine={this.DeleteLine}/>
+            {this.state.lines.map((data, i) => <Line key={i} id={i} GetCharacter={this.GetCharacter} DeleteLine={this.DeleteLine} character={data.character} dialog={data.dialog} emotion={data.emotion}/> ) }
         </div>
         <div className="dialog-opts">
             <button className="btn btn-add" onClick={this.AddLine}> Add line </button>
--- src/components/line.js
+++ src/components/line.js
@@ -1,23 +1,34 @@
 import React from 'react';
- 
+
+const opts = {
+    options: [
+      { name: 'Select…', value: null },
+      { name: 'Annie', value: 'Annie'},
+      { name: 'Machia', value: 'Machia'},
+      { name: 'Mr. Gray', value: 'Mr. Gray'}
+    ]
+  }
+  
 const Line = (props) => {
+ 
+
     return (
     <div className="line">
         <div className="input-name"> 
-            <select>
-                <option value="0">Character Name</option>
-                <option value="saab">Annie</option>
-                <option value="mercedes">Machia</option>
-                <option value="audi">Mr. Gray</option>
+            <select onChange={props.GetCharacter} value={props.character}> 
+                {opts.options.map(item => ( <option key={item.value} value={item.value}> {item.name} </option> ))}
+                <option value="Annie">Annie</option>
+                <option value="Machia">Machia</option>
+                <option value="Mr. Gray">Mr. Gray</option>
             </select> 
-            <button className="btn btn-delete" onClick={props.DeleteLine}> Delete line </button>
+            <button className="btn btn-delete" onClick={() => {props.DeleteLine(props.id)}}> Delete line </button>
         </div>
         <textarea rows="5"></textarea>
         <div className="expressions">
             <input type="checkbox" name="expression" value="Default" checked /> Default 
             <input type="checkbox" name="expression" value="Angry" /> Angry 
             <input type="checkbox" name="expression" value="Happy" /> Happy 
-            <input type="checkbox" name="expression" value="Angry" /> Angry 
+            <input type="checkbox" name="expression" value="Angry" /> Worried 
             <input type="checkbox" name="expression" value="Amazed" /> Amazed 
             <input type="checkbox" name="expression" value="Scared" /> Scared 
             <input type="checkbox" name="expression" value="Laughing" /> Laughing 
