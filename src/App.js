fdb4e64aa3d8528e13643bf8753b28e87acd21e9
Joorch <jacortez_94@hotmail.com>
2020-02-21 14:54:55 -0600

    maybe it'll work soon _

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
 import React, { Component } from 'react';
-import Dialog from './components/dialog';
-import Nav from './components/nav';
+import Dialog from './components/dialog'; 
+import Characters from './components/characters';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Nav />
-        <Dialog />
+      <div className="App"> 
+        <Router> 
+            <div className="nav-bar">
+              <li>
+                <Link className="nav-item" to="/">New Conversation</Link>
+              </li>
+              <li>
+                <Link className="nav-item" to="/Characters">Manage Characters</Link>
+              </li>
+              <li>
+                <Link className="nav-item" to="/Moods">Manage moods</Link>
+              </li>
+            </div> 
+            <Switch>
+              <Route path="/">
+                <Dialog />
+              </Route>
+              <Route path="/Characters">
+                <Characters />
+              </Route>
+            </Switch> 
+        </Router>
       </div>
     );
   }
 }
 
 export default App;
+
