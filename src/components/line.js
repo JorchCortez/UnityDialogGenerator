0414a4f0e55c26c0cbaef6575d35abc3b3e63564
Joorch <jacortez_94@hotmail.com>
2020-02-27 07:21:58 -0600

    Adding missing moods

--- src/components/line.js
+++ src/components/line.js
@@ -34,11 +34,11 @@ class Line extends React.Component {
         return (
             <div className="line">
                 <div className="opts dialog-opts">
-                <h1 className="dialog-title">Dialog line #{this.props.id + 1}</h1><button className="btn btn-delete" onClick={() => {this.props.DeleteLine(this.props.id)}}> Delete line </button>
+                    <h1 className="dialog-title">Dialog line #{this.props.id + 1}</h1><button className="btn btn-delete" onClick={() => {this.props.DeleteLine(this.props.id)}}> Delete line </button>
                 </div>
                 <div className="input-name"> 
                     <select id={this.props.id} onChange={this.props.GetCharacter} value={this.props.character}> 
-        <option value=""> {(this.state.characters.length > 0) ? "Select a character" : "Please add characters" } </option>
+                        <option value=""> {(this.state.characters.length > 0) ? "Select a character" : "Please add characters" } </option>
                         {this.state.characters.map((c, i) => ( <option key={i} value={c.name}> {c.name} </option> ))} 
                     </select> 
                     <select id={this.props.id} onChange={this.props.GetEmotion} value={this.props.emotion}> 
